1263156ee7eb2e5d56b59106c8c68477501c0c54
Tomas Kormanak <kormik@kormik.net>
2012-10-09 09:44:47 +0200

    Added servicelocator to CMSFrontController.

--- module/Vivo/src/Vivo/Controller/CMSFrontController.php
+++ module/Vivo/src/Vivo/Controller/CMSFrontController.php
@@ -2,23 +2,35 @@
 namespace Vivo\Controller;
 
 use Zend\EventManager\EventInterface as Event;
+use Zend\Http\Response as HttpResponse;
 use Zend\Mvc\InjectApplicationEventInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Stdlib\DispatchableInterface;
 use Zend\Stdlib\RequestInterface as Request;
 use Zend\Stdlib\ResponseInterface as Response;
-use Zend\Http\Response as HttpResponse;
 
 /**
  * The front controller which is responsible for dispatching all requests for documents and files in CMS repository.
  * @author kormik
  */
-class CMSFrontController implements DispatchableInterface, InjectApplicationEventInterface {
+class CMSFrontController implements DispatchableInterface, InjectApplicationEventInterface, ServiceLocatorAwareInterface {
 
 	/**
 	 * @var Zend\Mvc\MvcEvent
 	 */
 	protected $event;
 	
+	/**
+	 * @var Zend\ServiceManager\ServiceManager
+	 */
+	private $serviceLocator;
+	
+	/**
+	 * @param Request $request
+	 * @param Response $response
+	 * @return Response
+	 */
 	public function dispatch(Request $request, Response $response = null) {
 		//TODO find document in repository and return it
 		$path = $this->event->getRouteMatch()->getParam('path');
@@ -27,11 +39,31 @@ class CMSFrontController implements DispatchableInterface, InjectApplicationEven
 		return $response;
 	}
 	
+	/**
+	 * @param Event $event
+	 */
 	public function setEvent(Event $event) {
 		$this->event = $event;
 	}
 	
+	/**
+	 * @return \Zend\Mvc\MvcEvent
+	 */
 	public function getEvent() {
 		return $this->event;
+	}
+	
+	/**
+	 * @param ServiceLocatorInterface $serviceLocator
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator) {
+		$this->serviceLocator = $serviceLocator;
+	}
+	
+	/**
+	 * @return \Zend\ServiceManager\ServiceManager
+	 */
+	public function getServiceLocator() {
+		return $this->serviceLocator;
 	}
 }

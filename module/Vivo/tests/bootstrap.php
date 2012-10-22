a4ec585a74bd533da7c37208e40d33396ed26774
David Lukas <david.lukas@zfdaily.com>
2012-10-22 15:03:26 +0200

    Testing bootstrap - fix previous changes commited by mistake

--- module/Vivo/tests/bootstrap.php
+++ module/Vivo/tests/bootstrap.php
@@ -1,30 +1,21 @@
 <?php
-//Ini settings
-ini_set('display_startup_errors', true);
-ini_set('display_errors', true);
-error_reporting( E_ALL | E_STRICT );
+error_reporting(E_ALL | E_STRICT);
 
-//ZF2 Path is taken form the ZF2_PATH environment variable
-$zf2Path    = getenv('ZF2_PATH');
-if (!$zf2Path) {
-    throw new \RuntimeException("Test bootstrap: Environment variable 'ZF2_PATH' not found."
-                                . ' Set this variable to point to the ZF2 library and run the test again.');
+if (class_exists('PHPUnit_Runner_Version', true)) {
+    $phpUnitVersion = PHPUnit_Runner_Version::id();
+    if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '3.6.0', '<')) {
+        echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported in Zend Framework 2.x unit tests.' . PHP_EOL;
+        exit(1);
+    }
+    unset($phpUnitVersion);
 }
 
-//Paths to other modules for which autoloading has to be set up
-$otherModulePaths   = array(
-
-);
+include realpath(__DIR__.'/../../../init_autoloader.php');
 
 chdir(dirname(__DIR__));
 
-//ZF2 Autoloader
-require_once ($zf2Path . '/Zend/Loader/AutoloaderFactory.php');
-Zend\Loader\AutoloaderFactory::factory(array(
-                                           'Zend\Loader\StandardAutoloader' => array(
-                                               'autoregister_zf' => true
-                                           )
-                                       ));
+//Paths to other modules for which autoloading has to be set up
+$otherModulePaths = array();
 
 /**
  * Configures autoloading as defined in the module
@@ -34,21 +25,21 @@ Zend\Loader\AutoloaderFactory::factory(array(
  */
 function configureAutoloaderForModule($modulePath) {
     $modulePath = realpath($modulePath);
-    $namespace  = basename($modulePath);
+    $namespace = basename($modulePath);
 
-    //Autoloader for source of the module
-    $moduleFile             = $modulePath . '/Module.php';
-    $moduleClass            = $namespace . '\Module';
+//Autoloader for source of the module
+    $moduleFile = $modulePath . '/Module.php';
+    $moduleClass = $namespace . '\Module';
     require_once($moduleFile);
-    $module                 = new $moduleClass();
-    $autoloadConfig         = $module->getAutoloaderConfig();
-    \Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
+    $module = new $moduleClass();
+    $autoloadConfig = $module->getAutoloaderConfig();
+    Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
 
-    //Autoloader for tests of the module
-    $autoloadConfigFile     = $modulePath . '/tests/autoload_config_test.php';
+//Autoloader for tests of the module
+    $autoloadConfigFile = $modulePath . '/tests/autoload_config_test.php';
     if(file_exists($autoloadConfigFile)) {
-        $autoloadConfig     = require $autoloadConfigFile;
-        \Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
+        $autoloadConfig = require $autoloadConfigFile;
+        Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
     }
 }
 
@@ -62,4 +53,4 @@ foreach($otherModulePaths as $otherModulePath) {
 }
 
 //Unset local vars
-unset($zf2Path, $thisModulePath, $otherModulePath, $otherModulePaths);
+unset($thisModulePath, $otherModulePath, $otherModulePaths);

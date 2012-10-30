bb7259f4fed69e42d35ced856ba5fb40575b05ed
Tomas Kormanak <kormik@kormik.net>
2012-10-30 16:36:00 +0100

    Added loading Vivo core resources. WIP

--- module/Vivo/src/Vivo/Controller/ResourceFrontController.php
+++ module/Vivo/src/Vivo/Controller/ResourceFrontController.php
@@ -1,6 +1,11 @@
 <?php
 namespace Vivo\Controller;
 
+use Vivo\Controller\Exception;
+use Vivo\IO\Exception\ExceptionInterface  as IOException;
+
+use Vivo\IO\FileInputStream;
+
 use Zend\Mvc\InjectApplicationEventInterface;
 use Zend\Stdlib\DispatchableInterface;
 use Zend\EventManager\EventInterface as Event;
@@ -8,30 +13,54 @@ use Zend\Stdlib\RequestInterface as Request;
 use Zend\Stdlib\ResponseInterface as Response;
 use Zend\Http\Response as HttpResponse;
 
-
 /**
  * Controller for giving all resource files
- * @author kormik
  */
-class ResourceFrontController implements DispatchableInterface, InjectApplicationEventInterface {
+class ResourceFrontController implements DispatchableInterface,
+    InjectApplicationEventInterface
+{
 
-	protected $event;
+    /**
+     * @var Event
+     */
+    protected $event;
 
-	public function dispatch(Request $request, Response $response = null) {
-		//TODO find resource file by path and return it
-	//	throw new \Exception();
-		$path = $this->event->getRouteMatch()->getParam('path');
-		$module = $this->event->getRouteMatch()->getParam('module');
-		$response->setContent("Resource file in module: '$module' path: '$path' ");
-		$response->setStatusCode(HttpResponse::STATUS_CODE_200);
-		return $response;
-	}		
-	
-	public function setEvent(Event $event) {
-		$this->event = $event;
-	}
-	
-	public function getEvent() {
-		return $this->event;
-	}
+    public function dispatch(Request $request, Response $response = null)
+    {
+        //TODO find resource file by path and return it
+        $path = $this->event->getRouteMatch()->getParam('path');
+        $module = $this->event->getRouteMatch()->getParam('module');
+
+        //TODO set apropriate headers
+
+        if ($module === 'vivo') {
+            //it's vivo core resource
+            try {
+                $resourceStream = new FileInputStream(__DIR__.'/../../../resources/'.$path);
+            } catch (IOException $e) {
+                throw new Exception\FileNotFoundException("Resource file not found.", null, $e);
+            }
+        } else {
+            //TODO load resource from module
+        }
+
+        $response->setStream($resourceStream);
+        return $response;
+    }
+
+    /* (non-PHPdoc)
+     * @see Zend\Mvc.InjectApplicationEventInterface::setEvent()
+     */
+    public function setEvent(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    /* (non-PHPdoc)
+     * @see Zend\Mvc.InjectApplicationEventInterface::getEvent()
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
 }

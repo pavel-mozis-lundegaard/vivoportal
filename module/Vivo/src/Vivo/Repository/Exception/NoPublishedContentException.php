d4c0e8bb20456623ddf274e3b3b1cffc96876228
Miroslav Hajek <mhajek@lundegaard.cz>
2012-10-31 16:09:58 +0100

    Exception facelift

--- module/Vivo/src/Vivo/Repository/Exception/NoPublishedContentException.php
+++ module/Vivo/src/Vivo/Repository/Exception/NoPublishedContentException.php
@@ -3,15 +3,5 @@ namespace Vivo\Repository\Exception;
 
 /**
  * Document has no published content.
- * @author miroslav.hajek
  */
-class NoPublishedContentException extends \Exception implements ExceptionInterface {
-
-	/**
-	 * @param string $ident Entity identification (path, UUID or symbolic reference).
-	 */
-// 	public function __construct($ident) {
-// 		parent::__construct(403, 'no_published_content', array($ident));
-// 	}
-
-}
+class NoPublishedContentException extends \Exception implements ExceptionInterface { }

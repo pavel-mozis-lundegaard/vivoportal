062e821878a257488557066bfb8c1a7cdcab06fc
David Lukas <david.lukas@zfdaily.com>
2013-02-20 14:32:32 +0100

    Workflow factory clean-up

--- module/Vivo/src/Vivo/CMS/Workflow/Factory.php
+++ module/Vivo/src/Vivo/CMS/Workflow/Factory.php
@@ -11,28 +11,11 @@ use Zend\ServiceManager\ServiceManager;
  */
 class Factory
 {
-//    /**
-//     * Factory options
-//     * @var array
-//     */
-//    protected $options  = array(
-//        //Workflow names mapped to workflow classes
-//        'classMap'  => array(
-//            'workflow_basic'    => 'Vivo\CMS\Workflow\Basic',
-//        ),
-//    );
-
     /**
      * Service Manager
      * @var ServiceManager
      */
     protected $sm;
-
-//    /**
-//     * Workflows instantiated so far
-//     * @var WorkflowInterface[]
-//     */
-//    protected $workflows    = array();
 
     /**
      * Constructor
@@ -51,34 +34,7 @@ class Factory
      */
     public function get($name)
     {
-//        $name   = $this->getWorkflowServiceName($name);
         $workflow   = $this->sm->get($name);
         return $workflow;
-//        if (!array_key_exists($name, $this->workflows)) {
-//            if (!isset($this->options['classMap'][$name])) {
-//                throw new Exception\InvalidArgumentException(
-//                    sprintf("%s: Unknown workflow name '%s'", __METHOD__, $name));
-//            }
-//            $class                  = $this->options['classMap'][$name];
-//            $workflow               = new $class();
-//            $this->workflows[$name] = $workflow;
-//        }
-//        return $this->workflows[$name];
     }
-
-//    /**
-//     * Returns normalized workflow name (converts workflow class to name)
-//     * @param string $name
-//     * @return string
-//     */
-//    protected function getWorkflowServiceName($name)
-//    {
-//        if (array_key_exists($name, $this->options['classMap'])) {
-//            return $name;
-//        }
-//        if (in_array($name, $this->options['classMap'])) {
-//            return array_search($name, $this->options['classMap']);
-//        }
-//        return $name;
-//    }
 }

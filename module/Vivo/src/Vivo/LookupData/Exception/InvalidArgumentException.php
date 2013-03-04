9f126167de12ea943769a1b5dae2477bcd8bc798
Miroslav Hajek <mhajek@lundegaard.cz>
2013-03-04 15:54:42 +0100

    Not used

--- module/Vivo/src/Vivo/LookupData/Exception/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Vivo\LookupData\Exception;
-
-/**
- * InvalidArgumentException
- */
-class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
-{
-}

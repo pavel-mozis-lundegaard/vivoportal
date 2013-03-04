f59396547f072627009208596070c96917a9e1cc
Miroslav Hajek <mhajek@lundegaard.cz>
2013-03-04 15:51:49 +0100

    EntityResource

--- module/Vivo/src/Vivo/Backend/Provider/EntityResource.php
+++ module/Vivo/src/Vivo/Backend/Provider/EntityResource.php
@@ -1,15 +1,26 @@
 <?php
 namespace Vivo\Backend\Provider;
 
-use Vivo\LookupData\LookupDataProviderInterface;
+use Vivo\LookupData\AbstractLookupDataProvider;
 use Vivo\CMS\Model\Entity;
 
-class EntityResource implements LookupDataProviderInterface
+class EntityResource extends AbstractLookupDataProvider
 {
-
+    /**
+     * @return array
+     */
     public function getLookupData($property, array $propertyMetadata, Entity $entity)
     {
-        return array(1 , 2, strtoupper($property) => $property);
+        $return = array('' => '');
+
+        /* @var $cms \Vivo\CMS\Api\CMS */
+        $cms = $this->serviceManager->get('Vivo\CMS\Api\CMS');
+
+        foreach ($cms->scanResources($entity) as $name) {
+            $return[$name] = $name;
+        }
+
+        return $return;
     }
 
 }

d9d3049433e13c0d45fa4e48259f34d2943f777c
David Lukas <david.lukas@zfdaily.com>
2013-04-10 12:55:22 +0200

    VP-91 Form class fixes

--- src/Vivo/Form/Form.php
+++ src/Vivo/Form/Form.php
@@ -2,10 +2,14 @@
 namespace Vivo\Form;
 
 use Vivo\Form\Factory;
+use Vivo\InputFilter\VivoInputFilter;
 
 use Zend\Form\Form as ZendForm;
 use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterProviderInterface;
 use Zend\InputFilter\InputFilterInterface;
+use Zend\Form\FieldsetInterface;
+use Zend\InputFilter\InputProviderInterface;
 
 class Form extends ZendForm
 {
@@ -31,7 +35,7 @@ class Form extends ZendForm
             } else {
                 $name = $this->baseFieldset->getName();
                 if (!$this->filter instanceof InputFilterInterface || !$this->filter->has($name)) {
-                    $filter = new \Vivo\InputFilter\VivoInputFilter();
+                    $filter = new VivoInputFilter();
                     $filter->add($this->object->getInputFilter(), $name);
                     $this->filter = $filter;
                 }
@@ -40,7 +44,7 @@ class Form extends ZendForm
 
         if (!isset($this->filter)) {
             //Create VivoInputFilter by default
-            $this->filter = new \Vivo\InputFilter\VivoInputFilter();
+            $this->filter = new VivoInputFilter();
         }
 
         if (!$this->hasAddedInputFilterDefaults
@@ -52,5 +56,92 @@ class Form extends ZendForm
         }
 
         return $this->filter;
+    }
+
+    /**
+     * Attach defaults provided by the elements to the input filter
+     *
+     * @param  InputFilterInterface $inputFilter
+     * @param  FieldsetInterface $fieldset Fieldset to traverse when looking for default inputs
+     * @return void
+     */
+    public function attachInputFilterDefaults(InputFilterInterface $inputFilter, FieldsetInterface $fieldset)
+    {
+        $formFactory  = $this->getFormFactory();
+        $inputFactory = $formFactory->getInputFilterFactory();
+
+        if ($this instanceof InputFilterProviderInterface) {
+            $inputFilterSpecification   = $this->getInputFilterSpecification();
+            if ($inputFactory instanceof \Vivo\InputFilter\Factory) {
+                $inputFactory->addConditionsFromSpecification($inputFilter, $inputFilterSpecification);
+            }
+            foreach ($inputFilterSpecification as $name => $spec) {
+                $input = $inputFactory->createInput($spec);
+                $inputFilter->add($input, $name);
+            }
+        }
+
+        foreach ($fieldset->getElements() as $element) {
+            $name = $element->getName();
+
+            if ($this->preferFormInputFilter && $inputFilter->has($name)) {
+                continue;
+            }
+
+            if (!$element instanceof InputProviderInterface) {
+                if ($inputFilter->has($name)) {
+                    continue;
+                }
+                // Create a new empty default input for this element
+                $spec = array('name' => $name, 'required' => false);
+            } else {
+                // Create an input based on the specification returned from the element
+                $spec  = $element->getInputSpecification();
+            }
+
+            $input = $inputFactory->createInput($spec);
+            $inputFilter->add($input, $name);
+        }
+
+        foreach ($fieldset->getFieldsets() as $fieldset) {
+            $name = $fieldset->getName();
+
+            if (!$fieldset instanceof InputFilterProviderInterface) {
+                if (!$inputFilter->has($name)) {
+                    // Add a new empty input filter if it does not exist (or the fieldset's object input filter),
+                    // so that elements of nested fieldsets can be recursively added
+                    if ($fieldset->getObject() instanceof InputFilterAwareInterface) {
+                        $inputFilter->add($fieldset->getObject()->getInputFilter(), $name);
+                    } else {
+                        $inputFilter->add(new VivoInputFilter(), $name);
+                    }
+                }
+
+                $fieldsetFilter = $inputFilter->get($name);
+
+                if (!$fieldsetFilter instanceof InputFilterInterface) {
+                    // Input attached for fieldset, not input filter; nothing more to do.
+                    continue;
+                }
+
+                // Traverse the elements of the fieldset, and attach any
+                // defaults to the fieldset's input filter
+                $this->attachInputFilterDefaults($fieldsetFilter, $fieldset);
+                continue;
+            }
+
+            if ($inputFilter->has($name)) {
+                // if we already have an input/filter by this name, use it
+                continue;
+            }
+
+            // Create an input filter based on the specification returned from the fieldset
+            $spec   = $fieldset->getInputFilterSpecification();
+            $filter = $inputFactory->createInputFilter($spec);
+            $inputFilter->add($filter, $name);
+
+            // Recursively attach sub filters
+            $this->attachInputFilterDefaults($filter, $fieldset);
+        }
     }
 }

7451c715de06186ae45b0f11e8a6132acd284f6a
David Lukas <david.lukas@zfdaily.com>
2013-04-25 17:44:52 +0200

    VP-109 Minor clean-up

--- src/Vivo/CMS/ComponentFactory.php
+++ src/Vivo/CMS/ComponentFactory.php
@@ -112,10 +112,8 @@ class ComponentFactory implements EventManagerAwareInterface
                 $frontComponent->getView()->setTemplate($document->getContentContainerTemplate());
             }
 
-//            $i = 1;
             foreach ($contents as $contentContainerName => $content) {
                 $cc = $this->getContentFrontComponent($content, $document);
-//                $frontComponent->addComponent($cc, 'content' . $i++);
                 $frontComponent->addComponent($cc, $contentContainerName);
             }
 

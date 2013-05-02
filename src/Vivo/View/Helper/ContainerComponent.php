a9c7014a8ae325630ef0d2f7f72770ba93a83a0b
David Lukas <david.lukas@zfdaily.com>
2013-05-02 14:33:16 +0200

    VP-109 Added option to inject component view model to layout, ContainerComponent view helper modified

--- src/Vivo/View/Helper/ContainerComponent.php
+++ src/Vivo/View/Helper/ContainerComponent.php
@@ -2,97 +2,145 @@
 namespace Vivo\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
+use Zend\View\Model\ModelInterface as ViewModelInterface;
 
 /**
  * ContainerComponent
  * Returns rendered component from component container
- * This view helper is usable only in view templates of Vivo\UI\ComponentContainer
- */
+  */
 class ContainerComponent extends AbstractHelper
 {
     /**
      * Invokes the view helper as renderer method
      * @param int|string|null $componentId Int = component number, String = component name, null = return this helper
-     * @param string|null $defaultContent Default content displayed when the specified component is not available
-     * NOTE: the $defaultContent must be already escaped!
+     * @param mixed $viewModel If null, uses the current view model
      * @return ContainerComponent|string
      */
-    public function __invoke($componentId = null, $defaultContent = null)
+    public function __invoke($componentId = null, $viewModel = null)
     {
         if (is_null($componentId)) {
             return $this;
         }
-        return $this->render($componentId, $defaultContent);
+        return $this->render($componentId, $viewModel);
     }
 
     /**
      * Returns rendered component with the specified ID
      * @param int|string $componentId Int = component number, String = component name
-     * @param string|null $defaultContent Default content displayed when the specified component is not available
-     * NOTE: the $defaultContent must be already escaped!
+     * @param mixed $viewModel If null, uses the current view model
      * @throws Exception\InvalidArgumentException
      * @return string
      */
-    public function render($componentId, $defaultContent = null)
+    public function render($componentId, $viewModel = null)
     {
-        if (!isset($this->getView()->container['components'])) {
-            //The view does not contain the 'container' variable, probably not called from ComponentContainer template
+        if (!$viewModel) {
+            $viewModel  = $this->getView();
+        }
+        if (!$this->containerExists($viewModel)) {
+            //The view model does not contain a container
             return sprintf("<!-- %s: Container not found in view -->", __METHOD__);
         }
-        $components = $this->getView()->container['components'];
-        if (is_int($componentId)) {
-            //Component ID specified as integer, get component at this position
-            if (array_key_exists($componentId, $components)) {
-                //Component found
+        $components = $viewModel->container['components'];
+        if ($this->componentExists($componentId, $viewModel)) {
+            //Component found
+            if (is_int($componentId)) {
+                //Component ID specified as integer, get component at this position
                 $rendered   = sprintf("\n<!-- Component number %s -->\n", $componentId);
-                $rendered   .= $this->getView()->{$components[$componentId]};
-            } elseif ($defaultContent) {
-                //Component not found, but we have default content to display
-                $rendered   = sprintf("\n<!-- Component number %s not found, using default content -->\n",
-                                    $componentId);
-                $rendered   .= $defaultContent;
+                $rendered   .= $viewModel->{$components[$componentId]};
             } else {
-                //Component not found and default content is not set
-                $rendered   = sprintf('<!-- %s: Component number %s not found -->', __METHOD__, $componentId);
-            }
-        } elseif (is_string($componentId)) {
-            //Content ID specified as string, get content with this name
-            if (in_array($componentId, $components)) {
-                //Component found
+                //Content ID specified as string, get content with this name
                 $rendered   = sprintf("\n<!-- Component '%s' -->\n", $componentId);
-                $rendered   .= $this->getView()->{$componentId};
-            } elseif ($defaultContent) {
-                //Component not found, but we have default content to display
-                $rendered   = sprintf("\n<!-- Component '%s' not found, using default content -->\n",
-                                    $componentId);
-                $rendered   .= $defaultContent;
-            } else {
-                //Component not found and default content is not set
-                $rendered   = sprintf("<!-- %s: Component '%s' not found -->", __METHOD__, $componentId);
+                $rendered   .= $viewModel->{$componentId};
             }
         } else {
-            throw new Exception\InvalidArgumentException(
-                sprintf("%s: contentId must be either integer or string", __METHOD__));
+            //Component not found
+            $rendered   = sprintf("<!-- %s: Component '%s' not found -->", __METHOD__, $componentId);
         }
         return $rendered;
     }
 
     /**
      * Renders all components sequentially
+     * @param mixed $viewModel
      * @return string
      */
-    public function renderAll()
+    public function renderAll($viewModel = null)
     {
-        if (!isset($this->getView()->container['components'])) {
-            //The view does not contain the 'container' variable, probably not called from ComponentContainer template
+        if (!$viewModel) {
+            $viewModel  = $this->getView();
+        }
+        if (!isset($viewModel->container['components'])) {
+            //The view does not contain the 'container' variable
             return sprintf("<!-- %s: Container not found in view -->", __METHOD__);
         }
-        $components = $this->getView()->container['components'];
+        $components = $viewModel->container['components'];
         $rendered   = sprintf("\n<!-- Rendering all components in container -->\n");
         foreach ($components as $componentNumber => $componentName) {
             $rendered   .= sprintf("<!-- Component %s, '%s' -->\n", $componentNumber, $componentName);
-            $rendered   .= $this->getView()->{$componentName} . "\n";
+            $rendered   .= $viewModel->{$componentName} . "\n";
         }
         return $rendered;
+    }
+
+    /**
+     * Returns if component container exists in the view model
+     * @param mixed If null, uses the current view model
+     * @return bool
+     */
+    public function containerExists($viewModel = null)
+    {
+        if (!$viewModel) {
+            $viewModel  = $this->getView();
+        }
+        if (isset($viewModel->container['components'])) {
+            //A container exists in the view
+            $retVal = true;
+        } else {
+            //The view does not contain the 'container' variable
+            $retVal = false;
+        }
+        return $retVal;
+    }
+
+    /**
+     * Returns if the specified component exists in the component container
+     * @param int|string $componentId
+     * @param mixed $viewModel If null, uses the current view model
+     * @return bool
+     */
+    public function componentExists($componentId, $viewModel = null)
+    {
+        if (!$viewModel) {
+            $viewModel  = $this->getView();
+        }
+        if (!$this->containerExists($viewModel)) {
+            return false;
+        }
+        $components = $viewModel->container['components'];
+        if (is_int($componentId) && array_key_exists($componentId, $components)) {
+            return true;
+        }
+        if (is_string($componentId) && in_array($componentId, $components)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns an array with names of the container components
+     * @param mixed $viewModel
+     * @throws Exception\RuntimeException
+     * @return array
+     */
+    public function getContainerComponentNames($viewModel = null)
+    {
+        if (!$this->containerExists($viewModel)) {
+            throw new Exception\RuntimeException(sprintf("%s: Component container does not exist in view", __METHOD__));
+        }
+        if (!$viewModel) {
+            $viewModel  = $this->getView();
+        }
+        $components = $viewModel->container['components'];
+        return $components;
     }
 }

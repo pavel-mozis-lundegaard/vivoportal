8510bf666bbd1bc10263974efc64af7da3cd586e
Tomas Kormanak <kormik@kormik.net>
2013-05-12 16:04:00 +0200

    Replaced bootstrap for unit test, acording to zend documentation.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,58 +1,111 @@
 <?php
+namespace VivoTest;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Stdlib\ArrayUtils;
+use RuntimeException;
+
 error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
 
-if (class_exists('PHPUnit_Runner_Version', true)) {
-    $phpUnitVersion = PHPUnit_Runner_Version::id();
-    if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '3.6.0', '<')) {
-        echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported in Zend Framework 2.x unit tests.' . PHP_EOL;
-        exit(1);
+class Bootstrap
+{
+    protected static $serviceManager;
+    protected static $config;
+    protected static $bootstrap;
+
+    public static function init()
+    {
+        // Load the user-defined test configuration file, if it exists; otherwise, load
+        if (is_readable(__DIR__ . '/TestConfig.php')) {
+            $testConfig = include __DIR__ . '/TestConfig.php';
+        } else {
+            $testConfig = include __DIR__ . '/TestConfig.php.dist';
+        }
+
+        $zf2ModulePaths = array();
+
+        if (isset($testConfig['module_listener_options']['module_paths'])) {
+            $modulePaths = $testConfig['module_listener_options']['module_paths'];
+            foreach ($modulePaths as $modulePath) {
+                if (($path = static::findParentPath($modulePath)) ) {
+                    $zf2ModulePaths[] = $path;
+                }
+            }
+        }
+
+        $zf2ModulePaths  = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
+        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ?: (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
+
+        static::initAutoloader();
+
+        // use ModuleManager to load this module and it's dependencies
+        $baseConfig = array(
+            'module_listener_options' => array(
+                'module_paths' => explode(PATH_SEPARATOR, $zf2ModulePaths),
+            ),
+        );
+
+        $config = ArrayUtils::merge($baseConfig, $testConfig);
+
+        $serviceManager = new ServiceManager(new ServiceManagerConfig());
+        $serviceManager->setService('ApplicationConfig', $config);
+        $serviceManager->get('ModuleManager')->loadModules();
+
+        static::$serviceManager = $serviceManager;
+        static::$config = $config;
     }
-    unset($phpUnitVersion);
-}
 
-include realpath(__DIR__ . '/../../../init_autoloader.php');
+    public static function getServiceManager()
+    {
+        return static::$serviceManager;
+    }
 
-chdir(dirname(__DIR__));
+    public static function getConfig()
+    {
+        return static::$config;
+    }
 
-//Paths to other modules for which autoloading has to be set up
-$otherModulePaths = array(
-    __DIR__ . '/../../ZendSearch',
-);
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
 
-/**
- * Configures autoloading as defined in the module
- * Autoloading is configured for the module source
- * as well as for the module tests (if autoload_config_test.php is present)
- * @param string $modulePath
- */
-function configureAutoloaderForModule($modulePath) {
-    $modulePath = realpath($modulePath);
-    $namespace = basename($modulePath);
+        if (is_readable($vendorPath . '/autoload.php')) {
+            $loader = include $vendorPath . '/autoload.php';
+        } else {
+            $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
 
-    //Autoloader for source of the module
-    $moduleFile = $modulePath . '/Module.php';
-    $moduleClass = $namespace . '\Module';
-    require_once($moduleFile);
-    $module = new $moduleClass();
-    $autoloadConfig = $module->getAutoloaderConfig();
-    Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
+            if (!$zf2Path) {
+                throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+            }
 
-    //Autoloader for tests of the module
-    $autoloadConfigFile = $modulePath . '/tests/autoload_config_test.php';
-    if(file_exists($autoloadConfigFile)) {
-        $autoloadConfig = require $autoloadConfigFile;
-        Zend\Loader\AutoloaderFactory::factory($autoloadConfig);
+            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+
+        }
+
+        AutoloaderFactory::factory(array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'autoregister_zf' => true,
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                ),
+            ),
+        ));
+    }
+
+    protected static function findParentPath($path)
+    {
+        $dir = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            $dir = dirname($dir);
+            if ($previousDir === $dir) return false;
+            $previousDir = $dir;
+        }
+        return $dir . '/' . $path;
     }
 }
 
-//Autoloading for this module
-$thisModulePath = realpath(__DIR__ . '/../');
-configureAutoloaderForModule($thisModulePath);
-
-//Autoloading for other modules
-foreach($otherModulePaths as $otherModulePath) {
-    configureAutoloaderForModule($otherModulePath);
-}
-
-//Unset local variables
-unset($thisModulePath, $otherModulePath, $otherModulePaths);
+Bootstrap::init();

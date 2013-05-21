ce02a03e96d20f17a5df6cb8ba05b7e302c5cb11
David Lukas <david.lukas@zfdaily.com>
2013-05-21 11:39:39 +0200

    VP-174 Document 'published' property editable in BE

--- src/Vivo/Filter/DateTime.php
+++ src/Vivo/Filter/DateTime.php
@@ -7,9 +7,47 @@ class DateTime extends AbstractFilter
 {
     public function filter($value)
     {
-        $date = \DateTime::createFromFormat('Y.m.d', $value);
-        $date = ($date instanceof \DateTime) ? $date : null;
-
+        //Locale format full
+        $format = 'd.m.Y H:i:s|';
+        $date   = \DateTime::createFromFormat($format, $value);
+        //Locale format w/o seconds
+        if ($date === false) {
+            $format = 'd.m.Y H:i|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //Locale format date only
+        if ($date === false) {
+            $format = 'd.m.Y|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //Modified ISO format
+        if ($date === false) {
+            $format = 'Y-m-d H:i|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //ISO format full with timezone
+        if ($date === false) {
+            $format = 'Y-m-d\TH:i:sP';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //ISO format full w/o timezone
+        if ($date === false) {
+            $format = 'Y-m-d\TH:i:s|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //ISO format full w/o timezone and seconds
+        if ($date === false) {
+            $format = 'Y-m-d\TH:i|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        //ISO format date only
+        if ($date === false) {
+            $format = 'Y-m-d|';
+            $date   = \DateTime::createFromFormat($format, $value);
+        }
+        if (! $date instanceof \DateTime) {
+            $date = null;
+        }
         return $date;
     }
 }

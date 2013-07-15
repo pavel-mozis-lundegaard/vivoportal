c0e86d4ee9b6429bf66b22d266dbc852a97d4f89
Marek Juras <marek.juras@lundegaard.eu>
2013-07-15 11:21:38 +0200

    VP-5 added docblocks for url helper

--- src/Vivo/View/Helper/Url.php
+++ src/Vivo/View/Helper/Url.php
@@ -1,6 +1,8 @@
 <?php
 namespace Vivo\View\Helper;
 
+use Vivo;
+
 /**
  * Url view helper.
  *
@@ -9,15 +11,32 @@ namespace Vivo\View\Helper;
  */
 class Url extends \Zend\View\Helper\AbstractHelper
 {
+    /**
+     *
+     * @var Vivo\Util\UrlHelper
+     */
     protected $urlHelper;
 
+    /**
+     * Constructor
+     * @param Vivo\Util\UrlHelper $urlHelper
+     */
     public function __construct($urlHelper)
     {
         $this->urlHelper = $urlHelper;
     }
 
+    /**
+     * Assemble url using route and router params.
+     * @param string|null $name Route name
+     * @param array $params
+     * @param array $options
+     * @param bool $reuseMatchedParams
+     * @return string
+     */
     public function __invoke($name = null, array $params = array(), $options = array(), $reuseMatchedParams = false)
     {
-        return $this->urlHelper->fromRoute($name, $params, $options, $reuseMatchedParams);
+        $route = $this->urlHelper->fromRoute($name, $params, $options, $reuseMatchedParams);
+        return $route;
     }
 }
